f5178d3a111df2c9dd4a5fa9a0135cbeb70da3f1
nomadic4life <orbison.dennis@gmail.com>
2024-02-13 18:46:26 -0800

    implemented counter program

---
+++ programs/counter_program/src/lib.rs
@@ -0,0 +1,67 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3CAfH6HS7BM9vo7VmWMWdefwmw5j4gtJsmWt78GYTMEN");
+
+#[program]
+pub mod counter_program {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = ctx.accounts.user.key();
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count -= 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init, 
+        payer = user, 
+        space = 8 + 40, 
+        seeds = [user.key().as_ref(), b"counter"], 
+        bump
+    )]
+    pub counter: Account<'info, CounterAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, CounterAccount>,
+
+    pub authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, CounterAccount>,
+
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct CounterAccount {
+    pub authority: Pubkey,
+
+    pub count: u64,
+}
